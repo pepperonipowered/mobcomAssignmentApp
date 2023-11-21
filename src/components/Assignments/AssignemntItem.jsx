646aa7151827cb3cbdcf786a3f0b24f4b301b7b8
pepperonipowered <pepperonipowered@gmail.com>
2023-11-21 15:19:31 +0800

    asdfasdf

--- src/components/Assignments/AssignemntItem.jsx
+++ src/components/Assignments/AssignemntItem.jsx
@@ -1,10 +1,75 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Alert } from 'react-native'
+import { Text, Checkbox, Menu, IconButton, MD3Colors, Divider } from 'react-native-paper';
+import React, { useState } from 'react'
 
-const AssignemntItem = () => {
+const AssignemntItem = ({
+  navigation,
+  assignment
+}) => {
+  const [isChecked, setIsChecked] = useState(false)
+  const [visible, setVisible] = useState(false);
+  const openItemMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
   return (
-    <View>
-      <Text>AssignemntItem</Text>
+    <View style={{flex:1}}>
+      <Divider/>
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginHorizontal: 10, marginVertical: 5}}>
+        <View style={{ paddingHorizontal: 8 }}>
+          <Checkbox
+            // status={ isChecked ? 'checked' : 'unchecked' }
+            status={assignment.isCompleted}
+            onPress={() => setIsChecked(!isChecked)}
+            disabled={true}
+          />
+        </View>
+        <View style={{ flex: 1, borderLeftWidth: 2, paddingLeft: 20, borderLeftColor: MD3Colors.primary50 }}>
+          <Text variant='bodyMedium' style={{ marginRight: 10 }}>
+            {assignment.date}
+            
+          </Text>
+          <Text numberOfLines={1} ellipsizeMode='tail' variant='bodyLarge' style={{ fontWeight: 'bold', color: MD3Colors.primary50 }}>
+            {assignment.title}
+          </Text>
+        </View>
+
+        <Menu
+            visible={visible}
+            onDismiss={closeMenu}
+            anchor={
+              <IconButton
+                    icon="dots-horizontal"
+                    size={24}
+                    onPress={ openItemMenu }
+                    >
+                </IconButton>
+            }
+            anchorPosition='bottom'
+        >
+            <Menu.Item 
+                onPress={
+                  () => {
+                    navigation.navigate('Edit Assignment')
+                    closeMenu();
+                  }
+                } 
+                title="Edit" 
+                leadingIcon={'circle-edit-outline'}
+            />
+            <Divider/>
+            <Menu.Item 
+                onPress={()=>{}} 
+                title="Delete" 
+                leadingIcon={'trash-can-outline'}
+            />
+            <Divider/>
+            <Menu.Item 
+                onPress={closeMenu} 
+                title="Cancel" 
+                leadingIcon={'arrow-left-bottom'}
+            />
+        </Menu>
+      </View>
+      <Divider/>
     </View>
   )
 }
--- src/components/Assignments/AssignmentList.jsx
+++ src/components/Assignments/AssignmentList.jsx
@@ -1,12 +1,34 @@
-import { View } from 'react-native'
+import { View, FlatList } from 'react-native'
 import { Portal, FAB, Text } from 'react-native-paper'
 import React from 'react'
+import AssignemntItem from './AssignemntItem'
+// import { assignments } from '../../lib/mock_assignments'
 
+const assignments = [
+  {
+      id:1,
+      title:"Make It Happen",
+      description:"Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+      date:"4/28/2023",
+      mins:45,
+      isCompleted:true,
+      createdAt:"3/27/2023",
+      updatedAt:"11/7/2023"
+  },
+]
 const AssignmentList = ({
   navigation
 }) => {
   return (
     <View style={{ flex: 1, }}>
+        {/* <FlatList
+          data={assignments}
+          renderItem={(assignment) => <AssignemntItem navigation={navigation} assignment={assignment} />}
+          keyExtractor={assignment => assignment.id}
+        /> */}
+        {assignments.map((assignment) => (
+          <AssignemntItem navigation={navigation} assignment={assignment} />
+        ))}
         <Portal.Host>
           <View style={{ position: 'absolute', right: 0, bottom: 0, paddingBottom: 20, paddingRight: 20 }}>
             <FAB
@@ -15,6 +37,7 @@ const AssignmentList = ({
             />
           </View>
         </Portal.Host>
+        
     </View>
   )
 }
