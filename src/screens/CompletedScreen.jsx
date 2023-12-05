3533c920a8810f412429082b9ecf44c4a380601f
RonjayAcorda <rgacorda.the2nd@gmail.com>
2023-12-05 16:36:36 +0800

    expo prebuild

--- src/screens/CompletedScreen.jsx
+++ src/screens/CompletedScreen.jsx
@@ -1,7 +1,7 @@
 import { FlatList, View } from 'react-native'
 import { Portal, FAB, Text, Button, MD3Colors, ActivityIndicator } from 'react-native-paper'
 import React, { useEffect, useState } from 'react'
-import AssignmentItem from './AssignemntItem'
+import AssignmentItem from '../components/Assignments/AssignemntItem'
 import { FIREBASE_DB } from '../../../firebaseConfig'
 import { collection, query, onSnapshot, orderBy, where } from 'firebase/firestore'
 import CustomTopBar from '../components/CustomTopBar'
