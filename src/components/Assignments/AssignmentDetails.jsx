913b8c3c56bb2bca14f85acdfcf226f9d93415a5
RonjayAcorda <rgacorda.the2nd@gmail.com>
2023-12-07 10:08:42 +0800

    added assignment view

--- src/components/Assignments/AssignmentDetails.jsx
+++ src/components/Assignments/AssignmentDetails.jsx
@@ -1,4 +1,4 @@
-import { Keyboard, View, } from 'react-native'
+import { Keyboard, View, StyleSheet } from 'react-native'
 import { useEffect, useState} from 'react'
 import { TextInput, Button, MD3Colors, HelperText, Text } from 'react-native-paper'
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -9,7 +9,12 @@ import { collection, serverTimestamp, doc, getDocs, updateDoc, onSnapshot } from
 import { addAssignmentSchema } from '../../lib/form-schemas';
 import DropDownPicker from 'react-native-dropdown-picker';
 
+
+
 const AssignmentDetails = ({navigation, route}) => {
+
+  const [date, setDate] = useState(new Date())
+
   const { assignmentId, subjectId } = route.params
   const assignmentRef = doc(collection(FIREBASE_DB, 'assignments'), assignmentId)
   const subjectsRef = collection(FIREBASE_DB, 'subjects');
@@ -17,6 +22,8 @@ const AssignmentDetails = ({navigation, route}) => {
     const subscribe = onSnapshot( assignmentRef, (docSnapshot) => {
         if (docSnapshot.exists()) {
           setAssignment(docSnapshot.data())
+          setDate(new Date(docSnapshot.data().date.seconds * 1000 + docSnapshot.data().date.nanoseconds / 1000000))
+
         } else {
           console.log('Assignment does not exist')
         }
@@ -50,11 +57,63 @@ const AssignmentDetails = ({navigation, route}) => {
     value: ''
   }]);
 
+
+  const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff', // Or any suitable color
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: MD3Colors.primary, // Or any suitable color
+    marginBottom: 1,
+  },
+  subject: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: MD3Colors.primary, // Or any suitable color
+    marginBottom: 15,
+  },
+  description: {
+    fontSize: 15,
+    color: MD3Colors.primary, // Or any suitable color
+    marginBottom: 10,
+  },
+  DueDate: {
+    fontSize: 12,
+    fontFamily: 'monospace', // This sets the font to monospaced
+    color: 'white', // Sets the font color to white
+    marginBottom: 10,
+    backgroundColor: 'gray', // Sets the background color to gray
+    borderRadius: 10, // Rounds the corners
+    paddingHorizontal: 10, // Adds horizontal padding
+    paddingVertical: 5, // Adds vertical padding
+  },
+  Format:{
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: 'white', // Sets the font color to white
+    marginBottom: 10,
+  },
+
+
+});
   return (
-    <View>
-      <Text>{assignment.title}</Text>
-    </View>
+  <View style={styles.container}>
+    <Text style={styles.title}>{assignment.title}</Text>
+    <Text style={styles.subject}>{assignment.subject}</Text>
+      <Text style={styles.DueDate}>
+        {format(date, 'MMMM dd, yyyy')}
+        {assignment.paper ? ` - ` : ""}
+      <Text style={styles.Format}>{assignment.paper ? assignment.paper : ""}</Text>
+    </Text>
+
+    <Text style={styles.description}>{assignment.description}</Text>
+  </View>
   )
+  
 }
 
 export default AssignmentDetails
